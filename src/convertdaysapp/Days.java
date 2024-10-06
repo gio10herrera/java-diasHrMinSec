3627989953c21ecdc24936159d3bf7d3c920a3de
gio10herrera <herrerafontalvo@outlook.com>
2024-10-05 19:53:50 -0500

    conversion terminada

--- src/convertdaysapp/Days.java
+++ src/convertdaysapp/Days.java
@@ -3,9 +3,16 @@ package convertdaysapp;
 import javax.swing.*;
 
 public class Days {
-    static int numberOfDays;
+    static int numberOfDays, hours, minutes, seconds;
 
     public static void main(String[] args) {
         numberOfDays = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the number of days you want to convert", "Number of Days", JOptionPane.QUESTION_MESSAGE));
+        convertDays();
+    }
+
+    private static void convertDays() {
+        hours = numberOfDays * 24;
+        minutes = hours * 60;
+        seconds = minutes * 60;
     }
 }
